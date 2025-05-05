81e07c4b8b912b48d990c5dcff3a065346660863
Raman Kurai <ramankurai27@gmail.com>
2025-05-05 22:38:07 +0530

    update basic version of backend

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,10 +1,11 @@
 import { WebSocket } from "ws";
 import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./messages";
 
 export class GameManager {
     private games : Game[];
     private pendingUser : WebSocket | null;
-    private users :WebSocket[];
+    private users : WebSocket[];
 
     constructor() {
         this.games = [];
@@ -25,15 +26,21 @@ export class GameManager {
     private addHandler(socket : WebSocket){
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-            if (message.type === "join") {
+            if (message.type === INIT_GAME) {
                 if (this.pendingUser) {
-                    //start the game
                     const game = new Game(this.pendingUser , socket)
                     this.games.push(game);
                     this.pendingUser = null;
                 }
             } else {
                 this.pendingUser = socket;
+            }
+
+            if (message.type === MOVE) {
+                const game = this.games.find(game => game.player1 == socket || game.player2 == socket)
+                if (game) {
+                    game.makeMove (socket , message.move)
+                }
             }
         })
     }
--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -5,8 +5,10 @@ const wss = new WebSocketServer({port : 8080});
 
 const gameManager = new GameManager();
 
-wss.on('connection'  , function connection(ws){
+wss.on('connection', function connection(ws){
 gameManager.addUser(ws)
  
- ws.send('something');
+ws.on('disconnect' , () => {
+    gameManager.removeUser(ws)
+ });
 })
