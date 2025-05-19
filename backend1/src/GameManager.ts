2984825646a40f34e7f69ae02f8b8c683c0f2a34
Raman Kurai <ramankurai27@gmail.com>
2025-05-19 23:37:09 +0530

    fix : the init game and moves logic

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -13,6 +13,7 @@ export class GameManager {
     }
 
     addUser(socket : WebSocket) {
+      console.log("User Added")
         this.users.push(socket);
         this.addHandler(socket);
     }
@@ -24,27 +25,36 @@ export class GameManager {
 
     private addHandler(socket : WebSocket){
         socket.on("message", (data) => {
-            const message = JSON.parse(data.toString());
-            if (message.type === INIT_GAME) {
-                console.log("Received INIT_GAME");
-                if (this.pendingUser) {
-                    console.log("Pairing players...");
-                    const game = new Game(this.pendingUser, socket);
-                    this.games.push(game);
-                    this.pendingUser = null;
-                    console.log("Game started!");
-                } else {
-                    this.pendingUser = socket;
-                    console.log("Waiting for opponent...");
-                }
+    try {
+        const message = JSON.parse(data.toString());
+        console.log("Incoming message:", message);
+
+        if (message.type === INIT_GAME) {
+            console.log("Received INIT_GAME");
+            if (this.pendingUser) {
+                console.log("Pairing players...");
+                const game = new Game(this.pendingUser, socket);
+                this.games.push(game);
+                this.pendingUser = null;
+                console.log("Game started!");
+            } else {
+                this.pendingUser = socket;
+                console.log("Waiting for opponent...");
             }
-            if (message.type === MOVE) {
-                console.log("Recieved MOVE")
-                const game = this.games.find(game => game.player1 == socket || game.player2 == socket)
-                if (game) {
-                    game.makeMove (socket , message.move)
-                }
+        } else if (message.type === MOVE) {
+            console.log("Received MOVE", message.move);
+            const game = this.games.find(g => g.player1 === socket || g.player2 === socket);
+            if (game) {
+                game.makeMove(socket, message.move);
+                console.log("Move processed.");
+            } else {
+                console.log("No game found for this player.");
             }
-        })
+        }
+    } catch (e) {
+        console.error("Error parsing or handling message:", e);
+    }
+});
+
     }
 }
